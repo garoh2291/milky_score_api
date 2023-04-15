dc8da20b9976861ce95885d9797f0a12737d9f39
Garnik Hovsepyan <garoh2291@gmail.com>
2023-04-15 23:23:24 +0400

    change timestamps

--- models/news.js
+++ models/news.js
@@ -1,32 +1,34 @@
 const mongoose = require("mongoose"),
   Schema = mongoose.Schema;
 
-const NewsSchema = new Schema({
-  title: {
-    am: {
-      type: String,
-      required: true,
+const NewsSchema = new Schema(
+  {
+    title: {
+      am: {
+        type: String,
+        required: true,
+      },
+      en: {
+        type: String,
+        required: true,
+      },
     },
-    en: {
+    description: {
+      am: {
+        type: String,
+        required: true,
+      },
+      en: {
+        type: String,
+        required: true,
+      },
+    },
+    image: {
       type: String,
       required: true,
     },
   },
-  description: {
-    am: {
-      type: String,
-      required: true,
-    },
-    en: {
-      type: String,
-      required: true,
-    },
-  },
-  image: {
-    type: String,
-    required: true,
-  },
-  timestamps: true,
-});
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("News", NewsSchema);
